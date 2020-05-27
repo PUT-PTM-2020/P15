c289ae6f71bd1c94b44ba926420136075032fe9b
Jordan Kondracki <48299208+Jor-Kon@users.noreply.github.com>
2020-05-27 12:12:28 +0200

    Add files via upload

---
+++ TCP Client/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace TCP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.LabelConnection = new System.Windows.Forms.Label();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.buttonLF = new System.Windows.Forms.Button();
+            this.buttonRF = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonForward.Enabled = false;
+            this.buttonForward.Location = new System.Drawing.Point(285, 97);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(214, 109);
+            this.buttonForward.TabIndex = 0;
+            this.buttonForward.TabStop = false;
+            this.buttonForward.Text = "FORWARD";
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.ForwardButton_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonLeft.Enabled = false;
+            this.buttonLeft.Location = new System.Drawing.Point(65, 262);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(214, 109);
+            this.buttonLeft.TabIndex = 1;
+            this.buttonLeft.TabStop = false;
+            this.buttonLeft.Text = "LEFT";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.LeftButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port:";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(38, 8);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIP.TabIndex = 6;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(179, 8);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPort.TabIndex = 7;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(285, 6);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 8;
+            this.buttonConnect.Text = "connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // LabelConnection
+            // 
+            this.LabelConnection.AutoSize = true;
+            this.LabelConnection.Location = new System.Drawing.Point(635, 13);
+            this.LabelConnection.Name = "LabelConnection";
+            this.LabelConnection.Size = new System.Drawing.Size(81, 13);
+            this.LabelConnection.TabIndex = 9;
+            this.LabelConnection.Text = "Not connected!";
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Enabled = false;
+            this.buttonDisconnect.Location = new System.Drawing.Point(366, 6);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.TabIndex = 10;
+            this.buttonDisconnect.Text = "disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // buttonLF
+            // 
+            this.buttonLF.Enabled = false;
+            this.buttonLF.Location = new System.Drawing.Point(65, 97);
+            this.buttonLF.Name = "buttonLF";
+            this.buttonLF.Size = new System.Drawing.Size(214, 109);
+            this.buttonLF.TabIndex = 11;
+            this.buttonLF.TabStop = false;
+            this.buttonLF.Text = "LF";
+            this.buttonLF.UseVisualStyleBackColor = true;
+            this.buttonLF.Click += new System.EventHandler(this.buttonLF_Click);
+            // 
+            // buttonRF
+            // 
+            this.buttonRF.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonRF.Enabled = false;
+            this.buttonRF.Location = new System.Drawing.Point(502, 97);
+            this.buttonRF.Name = "buttonRF";
+            this.buttonRF.Size = new System.Drawing.Size(214, 109);
+            this.buttonRF.TabIndex = 12;
+            this.buttonRF.TabStop = false;
+            this.buttonRF.Text = "RF";
+            this.buttonRF.UseVisualStyleBackColor = true;
+            this.buttonRF.Click += new System.EventHandler(this.buttonRF_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonRight.Enabled = false;
+            this.buttonRight.Location = new System.Drawing.Point(502, 262);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(214, 109);
+            this.buttonRight.TabIndex = 13;
+            this.buttonRight.TabStop = false;
+            this.buttonRight.Text = "RIGHT";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(744, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonRF);
+            this.Controls.Add(this.buttonLF);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.LabelConnection);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonForward);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label LabelConnection;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Button buttonLF;
+        private System.Windows.Forms.Button buttonRF;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TCP Client/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCP_Client
+{
+    public partial class Form1 : Form
+    {
+        StreamWriter writer;
+        StreamReader reader;
+        TcpClient client;
+        Thread t1;
+        string read = string.Empty;
+        string danger = "dg";
+        bool isConnected = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*inicjalizacja wątku odpowiedzialnego za sprawdzenie łączności z serwerem oraz 
+             sprawdzającego czy klient otrzymał komunikat o napotkaniu przeszkodzy*/
+            t1 = new Thread(new ThreadStart(sensor));
+        }
+
+        /*Sprawdzenie czy serwer wysłał pakiet z komunikatem oznaczającym, że pojazd napotkał przeszkodę*/
+        public void sensor()                                        
+        {
+            while(true)
+            {
+                while (isConnected == true)
+                {
+                    if (readData() == true)                         
+                    {
+                        /*Wyłączenie przednich przycisków*/
+                        buttonRF.Invoke(new Action(delegate ()
+                        {
+                            buttonRF.Enabled = false;
+                        }));
+                        buttonLF.Invoke(new Action(delegate ()
+                        {
+                            buttonLF.Enabled = false;
+                        }));
+                        buttonForward.Invoke(new Action(delegate ()
+                        {
+                            buttonForward.Enabled = false;
+                        }));
+                    }
+                    else                                            
+                    {
+                        /*Włączenie przednich przycisków*/
+                        buttonRF.Invoke(new Action(delegate ()
+                        {
+                            buttonRF.Enabled = true;
+                        }));
+                        buttonLF.Invoke(new Action(delegate ()
+                        {
+                            buttonLF.Enabled = true;
+                        }));
+                        buttonForward.Invoke(new Action(delegate ()
+                        {
+                            buttonForward.Enabled = true;
+                        }));
+                    }
+                    //writer.Write("");
+                    Thread.Sleep(200);
+
+                    /*Sprawdzenie czy klient nadal jest połączony z serwerem*/
+                    if (client.Connected == false)                  
+                    {
+                        MessageBox.Show("Lost connection with server!");
+                        buttonRF.Invoke(new Action(delegate ()
+                        {
+                            disconnect();
+                        }));
+                    }
+                }
+            }
+        }
+
+        bool readData()
+        {
+            try
+            {
+                read = string.Empty;
+                client.ReceiveTimeout = 1;
+                read = reader.ReadLine();
+                if (read == danger) return true;
+                else return false;
+            }
+            catch (System.IO.IOException)
+            {
+                return false;
+            }
+        }
+
+        void connect()
+        {
+            try
+            {
+                if (textBoxIP.Text == "") throw new System.FormatException();
+               
+                /*Ustawienie połączenia z serwerem*/
+                client = new TcpClient(textBoxIP.Text, int.Parse(textBoxPort.Text));      
+                writer = new StreamWriter(client.GetStream());
+                reader = new StreamReader(client.GetStream());
+                Thread.Sleep(1000);
+
+                if (client.Connected == true) MessageBox.Show("Connected"); 
+                string lbl = "Connected to: " + textBoxIP.Text + ":" + textBoxPort.Text;
+                LabelConnection.Text = lbl;
+                this.KeyPreview = true;
+                isConnected = true;
+
+                /*Włączenie przycisków umożliwiających kontrolowanie pojazdu oraz przycisku 'disconnect'*/
+                buttonForward.Enabled = true;
+                buttonLeft.Enabled = true;
+                buttonRight.Enabled = true;
+                buttonLF.Enabled = true;
+                buttonRF.Enabled = true;
+                buttonDisconnect.Enabled = true;
+
+                /*Wyłączenie przycisku 'connect'*/
+                buttonConnect.Enabled = false;
+
+                /*Wyłączenie pól tekstowych*/
+                textBoxIP.Enabled = false;
+                textBoxPort.Enabled = false;
+
+                client.SendBufferSize = 20;
+
+                /*Wystartowanie lub wznowienie działania wątku t1*/
+                if(t1.ThreadState == ThreadState.Suspended)
+                {
+                    t1.Resume();
+                }
+                else t1.Start();
+            }
+            catch (System.Net.Sockets.SocketException ex)
+            {
+                MessageBox.Show("Failed to connect!");
+
+            }
+            catch (System.FormatException ex)
+            {
+                MessageBox.Show("Wrong text format");
+            }
+        }
+
+        void disconnect()
+        {
+            /*Przerwanie połączenie z serwerem*/
+            client.Close();
+            writer.Close();
+            reader.Close();
+
+            if (client.Connected == false) MessageBox.Show("Disconnected");
+            LabelConnection.Text = "Not connected!";
+            this.KeyPreview = false;
+            isConnected = false;
+
+            /*Wyłączenie przycisków umożliwiających kontrolowanie pojazdu oraz przycisku 'disconnect'*/
+            buttonForward.Enabled = false;
+            buttonLeft.Enabled = false;
+            buttonRight.Enabled = false;
+            buttonLF.Enabled = false;
+            buttonRF.Enabled = false;
+            buttonDisconnect.Enabled = false;
+
+            /*Włączenie przycisku 'connect'*/
+            buttonConnect.Enabled = true;
+
+            /*Wyłączenie pól tekstowych*/
+            textBoxIP.Enabled = true;
+            textBoxPort.Enabled = true;
+
+            /*Zawieszenie działania wątku t1*/
+            t1.Suspend();  
+        }
+
+        /*Zdarzenie związane z wciśnięciem przycisku na klawiaturze*/
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+                if (e.KeyCode == Keys.W)
+                {
+                    buttonForward.PerformClick();
+                }
+                else if (e.KeyCode == Keys.A)
+                {
+                    buttonLeft.PerformClick();
+                }
+                else if (e.KeyCode == Keys.D)
+                {
+                    buttonRight.PerformClick();
+                }
+                else if (e.KeyCode == Keys.Q)
+                {
+                    buttonLF.PerformClick();
+                }
+                else if (e.KeyCode == Keys.E)
+                {
+                    buttonRF.PerformClick();
+                }
+            //System.Threading.Thread.Sleep(100);
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            connect();
+            label3.Focus();
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            disconnect();
+            label3.Focus();
+        }
+
+        private void ForwardButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writer.WriteLine("Forward");
+                writer.Flush();
+                label3.Focus();
+            }
+            catch(System.IO.IOException)
+            {
+                MessageBox.Show("Lost connection with server!");
+                disconnect();
+            }
+
+        }
+
+        private void LeftButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writer.WriteLine("Left");
+                writer.Flush();
+                label3.Focus();
+            }
+            catch(System.IO.IOException)
+            {
+                MessageBox.Show("Lost connection with server!");
+                disconnect();
+            }
+           }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writer.WriteLine("Right");
+                writer.Flush();
+                label3.Focus();
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("Lost connection with server!");
+                disconnect();
+            }
+        }
+
+        private void buttonLF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writer.WriteLine("LLL");
+                writer.Flush();
+                label3.Focus();
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("Lost connection with server!");
+                disconnect();
+            }
+        }
+
+        private void buttonRF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writer.WriteLine("PP");
+                writer.Flush();
+                label3.Focus();
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("Lost connection with server!");
+                disconnect();
+            }
+        }
+    }
+}
